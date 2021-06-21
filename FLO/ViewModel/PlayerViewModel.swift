773d6b508f7f1895f44b9a6c69a6cdd2c3521566
Danna <songda515@gmail.com>
2021-06-22 02:33:24 +0900

    [Player] ViewModel

---
+++ FLO/ViewModel/PlayerViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  PlayerViewModel.swift
+//  FLO
+//
+//  Created by Dain Song on 2021/06/19.
+//
+
+import Foundation
+
+class PlayerViewModel {
+
+    var apiManager: APIManger = APIManger<Music>()
+    var music: Observable<Music> = Observable(Music.EMPTY)
+    var imageData: Observable<Data> = Observable(Data())
+    
+    func fetchMusic() {
+        print("fetch music")
+        self.apiManager.getMusic { (music) in
+            self.music = Observable(music)
+            let imageURL = music.image
+            self.apiManager.loadImageData(url: imageURL) { (image) in
+                self.imageData = Observable(image)
+            }
+        }
+    }
+    
+}
